ff64968f8aeb1392953ffb1bc4f054c8dbe2bff9
ruthcastroalva11 <ruthcastroalva11@gmail.com>
2020-07-27 12:09:26 -0500

    casi fin

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ validar.onclick=validar_tarjeta;
 
 function validar_tarjeta(){
     let cardNumberid=document.getElementById("cardNumberid").value;
-    let owner = document.getElementById("owner").value;
+    let owner = document.getElementById("ownername").value;
     //let fecha = document.getElementById("").value;
     let cvv = document.getElementById("cvv").value;
 
@@ -14,18 +14,27 @@ function validar_tarjeta(){
     let cover=validator.maskify(cardNumberid);
     console.log(cover);
 
-    if (cardNumberid==='' || owner==='' || cvv===''){
+    if (cardNumberid==='' || ownername==='' || cvv===''){
         alert("Completar todos los campos");
         return;
     }
 
     let message=document.getElementById("mensaje");
+    let message2=document.getElementById("ownername");
+    let message3=document.getElementById("exito");
+    //let owner = document.getElementById("owner").value;
 
     if (result==true){
-        message.innerHTML= "<p>" + cover + "</p>";
+        message.innerHTML= "Número de Tarjeta:  " + cover;
+        message2.innerHTML= "Nombre del Titular: " + owner;
+        message3.innerHTML= "¡Muchas Gracias! "+ "Tu compra ha sido realizada con éxito";
         //message.innerHTML= "<p>Tarjeta válida</p>";
-        document.getElementById('formulario_total').style.display='none';
+        //document.getElementById('formulario_total').style.display='none';
+        document.getElementById('principal').style.display='none';
         document.getElementById('mensaje').style.display='block';
+        document.getElementById('ownername').style.display='block';
+        document.getElementById('exito').style.display='block';
+
       } else {
       alert("El número de tarjeta no es válido. Volver a Ingresar");
 }
